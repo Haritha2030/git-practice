58197b43a4c322517fb6afc83f14b1d0f7e4ce61
harithac <harithanallagadda4905@gmail.com>
2024-11-01 16:49:25 +0530

    conversation

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+PERSON1=deepu
+PERSON2=chinnu
 
-echo "Ramesh:: Hi SUuresh, How are you?"
-echo "Suresh: Hello Ramesh. I am fine. How are you doing"
-echo "Ramesh:: I am doing good Suresh. What's going on?"
-echo "Suresh:: I started learning Shell Script Ramesh"
+echo "$PERSON1:: Hi Suresh, How are you?"
+echo "$PERSON2: Hello $PERSON1. I am fine. How are you doing"
+echo "$PERSON1:: I am doing good $PERSON2. What's going on?"
+echo "$PERSON2:: I started learning Shell Script $PERSON1"
