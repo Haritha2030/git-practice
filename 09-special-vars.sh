d81bf336ae30932fbff0d6a3b676ff11e8697739
harithac <harithanallagadda4905@gmail.com>
2024-11-04 09:09:57 +0530

    conditions

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -2,4 +2,8 @@
 
 echo "All variables passed to the script: $@"
 echo "Number of variables passed: $#"
+echo "Script name: $0"
 echo "Current working directory:$PWD"
+echo "Home directory of currn user: $HOME"
+echo "PID of the script executing now: $$"
+
