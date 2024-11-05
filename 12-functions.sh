1652d6dd016201f3955e9b888ec11a569c5795b6
harithac <harithanallagadda4905@gmail.com>
2024-11-05 08:29:59 +0530

    function

--- 12-functions.sh
+++ 12-functions.sh
@@ -2,7 +2,9 @@
 
 USERID=$(id -u)
 #echo "User ID is $USERID"
-
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
@@ -13,10 +15,10 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then    
-        echo "$2 is...FAILED"
+        echo -e "$2 is...$R FAILED $N"
         exit 1
     else
-        echo "$2 is...SUCCESS"
+        echo -e "$2 is...$G SUCCESS $N"
     fi
 }
 
