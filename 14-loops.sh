42e54eddfb3d4175f5a15ea0f303fdd5d185dba3
harithac <harithanallagadda4905@gmail.com>
2024-11-05 17:10:06 +0530

    loops

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,6 +1,15 @@
 #!/bim/bash
 
-for i in 1 2 3 4 5 6 7 8 9 10
+
+# in normal
+# for i in 1 2 3 4 5 6 7 8 9 10
+# do
+#     echo $i
+# done
+
+# in script
+
+for i in {0..0}  #0-10
 do
     echo $i
 done
